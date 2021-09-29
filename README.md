96b78b0bb04385cf186890e518bb825113c1d06a
Nexus <tyfeng98@163.com>
2021-09-29 22:46:51 +0800

    release on github

---
+++ README.md
@@ -0,0 +1,22 @@
+# WSISE White-box SM4 
+
+White-box implementation of SM4 algorithm with Internal State Expansion ([WSISE](http://www.jcr.cacrnet.org.cn/CN/Y2020/V7/I3/358)).
+
+## Build
+
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
+
+## Run
+
+```
+$ ./WBSM4
+```
+
+## Included libraries
+1. [WBMatrix](https://github.com/Nexus-TYF/WBMatrix)<br>
+2. [Xiao-Lai-White-box-SM4](https://github.com/Nexus-TYF/Xiao-Lai-White-box-SM4)<br>
---
+++ include/wbsm4.h
@@ -0,0 +1,28 @@
+#ifndef _HWBSM4_H_
+#define _HWBSM4_H_
+#include "WBMatrix/WBMatrix.h"
+#include "sm4.h"
+
+#define GET32(pc)  (\
+((uint32_t)(pc)[0] << 24) ^\
+((uint32_t)(pc)[1] << 16) ^\
+((uint32_t)(pc)[2] <<  8) ^\
+((uint32_t)(pc)[3]))
+
+#define PUT32(st, ct)\
+(ct)[0] = (uint8_t)((st) >> 24);\
+(ct)[1] = (uint8_t)((st) >> 16);\
+(ct)[2] = (uint8_t)((st) >>  8);\
+(ct)[3] = (uint8_t)(st)
+
+Aff32 M[32][3];
+Aff32 C[32];
+Aff32 D[32];
+Aff32 SE[4];
+Aff32 FE[4];
+uint64_t Table[32][4][256];
+
+void printstate(unsigned char * in);
+void wbsm4_gen(uint8_t *key);
+void wbsm4_encrypt(unsigned char IN[], unsigned char OUT[]);
+#endif
